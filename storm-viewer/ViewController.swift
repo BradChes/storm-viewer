7e6a7562f96241cb9230672547ffa902aafd5719
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-01-26 10:42:15 +0000

    Initial project setup

---
+++ storm-viewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  storm-viewer
+//
+//  Created by Bradley Chesworth on 26/01/2020.
+//  Copyright © 2020 Brad Chesworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
